a88fc5c83fa1804501dc638ba650589d1bb9180e
bluesatyr <shawnevans.music@gmail.com>
2021-01-24 09:55:40 -0500

    add musixmatch api search function

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,22 @@
 
 
 // api call to music service to find media for returned songs
+function findSong(searchTerm) {
+    fetch(
+        'https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?apikey=b25dc0cb4ca787de37dc0e3f1137fe5f&f_has_lyrics&q_lyrics=' + searchTerm + '&f_lyrics_language=en&s_track_rating'
+    ).then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        // returns the first track in an array of tracks '[0]'
+        var songObj = response.message.body.track_list[0].track; 
+        var songName = songObj.track_name;
+        var artistName = songObj.artist_name;
+        console.log(songName, artistName);
+        var result = songName + " " + artistName;
+        return result
+    });
+};
 
 
 // save result to local storage
