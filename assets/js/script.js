80d01b9efad2eb9f6994c1abe6a3489fbaded34b
bluesatyr <shawnevans.music@gmail.com>
2021-01-26 20:08:51 -0500

    code update

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,7 +73,29 @@ function findSong(searchTerm) {
 };
                                              
 // api call to music service (YouTube or Spotify) to find media for returned songs
+// AMELIA'S API KEY
+var apiKey = 'AIzaSyAxnLvO9fU3ahdMfivmsavDwE4qCwhzBgE';
 
+//var searchBtn = document.getElementById("search");
+//var searchTerm = document.querySelector("#searchTerm").value;
+
+
+var fetchYoutube = function(term) {
+    fetch(
+        'https://www.googleapis.com/youtube/v3/search'
+        + '?part=snippet&maxResults=25'
+        + '&q=' + term
+        + '&key=' + apiKey
+    )
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        console.log(response);
+    })
+}
+
+searchBtn.addEventListener("click", fetchYoutube);
 /* render YouTube video to the DOM
 var renderMedia = function(youTubeLink){
     var mediaEl = '';
